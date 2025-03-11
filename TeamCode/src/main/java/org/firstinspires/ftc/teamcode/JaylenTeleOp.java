6a822036a5dae6d2b473972121fe057003b3b007
Ryanhall729 <ryan.hall@mullenmail.com>
2025-03-11 17:59:30 -0600

    added a way to use joysticks to control robot drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JaylenTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JaylenTeleOp.java
@@ -109,10 +109,13 @@ public class JaylenTeleOp extends OpMode
         armBottom.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armBottom.setDirection(DcMotorSimple.Direction.REVERSE);
         jaw.setDirection(DcMotorSimple.Direction.REVERSE);
+        //made it easier to code, reversing some wheel's directions
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        //stop arm when no power
         armTop.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         armBottom.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //made it so, when no power, wheels stop
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -142,14 +145,68 @@ public class JaylenTeleOp extends OpMode
     @Override
     public void start() {
         runtime.reset();
+
     }
 
     /*
      * Code to run REPEATEDLY after the driver hits START but before they hit STOP
      */
+
     @Override
     public void loop() {
         // Setup a variable for each drive wheel to save power level for telemetry
+
+            double max;
+
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral =  gamepad1.left_stick_x;
+            double yaw     =  gamepad1.right_stick_x;
+
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+            double leftFrontPower  = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower   = axial - lateral + yaw;
+            double rightBackPower  = axial + lateral - yaw;
+
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower  /= max;
+                rightFrontPower /= max;
+                leftBackPower   /= max;
+                rightBackPower  /= max;
+            }
+
+            // This is test code:
+            //
+            // Uncomment the following code to test your motor directions.
+            // Each button should make the corresponding motor run FORWARD.
+            //   1) First get all the motors to take to correct positions on the robot
+            //      by adjusting your Robot Configuration if necessary.
+            //   2) Then make sure they run in the correct direction by modifying the
+            //      the setDirection() calls above.
+            // Once the correct motors move in the correct direction re-comment this code.
+
+            /*
+            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
+            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
+            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
+            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
+            */
+
+            // Send calculated power to wheels
+            frontLeft.setPower(leftFrontPower);
+            frontRight.setPower(rightFrontPower);
+            backLeft.setPower(leftBackPower);
+            backRight.setPower(rightBackPower);
+
+            // Show the elapsed game time and wheel power.
 
         if(gamepad1.dpad_up)
         {
@@ -169,28 +226,8 @@ public class JaylenTeleOp extends OpMode
             armBottom.setPower(0);
         }
 
-        if(gamepad1.left_bumper)
         {
-            frontLeft.setPower(1);
-            frontRight.setPower(1);
-            backLeft.setPower(1);
-            backRight.setPower(1);
-        }
 
-        if(gamepad1.right_bumper)
-        {
-            frontLeft.setPower(-1);
-            frontRight.setPower(-1);
-            backLeft.setPower(-1);
-            backRight.setPower(-1);
-        }
-
-        else
-        {
-            frontLeft.setPower(0);
-            frontRight.setPower(0);
-            backLeft.setPower(0);
-            backRight.setPower(0);
         }
 
         switch (servoStates)
